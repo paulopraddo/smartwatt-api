f51a80ade8c68ed5646b2022975bc043676c055f
Paulo Prado <paulopraddo011@gmail.com>
2025-04-16 19:14:30 -0300

    first commit

---
+++ src/main/java/com/fiap/SmartWatt/SmartWattApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.SmartWatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWattApplication.class, args);
+	}
+
+}
